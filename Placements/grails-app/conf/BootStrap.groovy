dfc1c37e47a45a7486790446bd35d3115259cf77
David Gallagher <david.m.gallagher2@student.shu.ac.uk>
2012-04-30 20:58:00 +0100

    Created a new method in the placement opportunity controller that returns all open applications as either xml or json

--- Placements/grails-app/conf/BootStrap.groovy
+++ Placements/grails-app/conf/BootStrap.groovy
@@ -21,6 +21,8 @@ class BootStrap {
     
     def status7 = new Status(code:'Rejected', description:'Offer rejected').save();
     
+    def status8 = new Status(code:'Open', description:'Application is open to applicants').save();
+    
     def student1 = new Student(name:'Steve Williams', courseCode:'COMP', applications:'', notes:'').save();
     
     def student2 = new Student(name:'David Roberts', courseCode:'MCOMP', applications:'', notes:'').save();
@@ -31,21 +33,23 @@ class BootStrap {
     
     def student5 = new Student(name:'Sarah Taylor', courseCode:'COMP', applications:'', notes:'').save();
     
-    def placementOpportunity1 = new PlacementOpportunity(companyName:'ABC Design and Development', jobTitle:'Designer', status:status3).save();
+    def placementOpportunity1 = new PlacementOpportunity(companyName:'ABC Design and Development', jobTitle:'Designer', status:status1).save();
     
-    def placementOpportunity2 = new PlacementOpportunity(companyName:'Lloyds Telecommunication', jobTitle:'Database Administrator', status:status3).save();
+    def placementOpportunity2 = new PlacementOpportunity(companyName:'Lloyds Telecommunication', jobTitle:'Database Administrator', status:status8).save();
     
-    def placementOpportunity3 = new PlacementOpportunity(companyName:'Williams Development', jobTitle:'Programmer', status:status3).save();
+    def placementOpportunity3 = new PlacementOpportunity(companyName:'Williams Development', jobTitle:'Programmer', status:status1).save();
+    
+    def placementOpportunity4 = new PlacementOpportunity(companyName:'ZYX Computing', jobTitle:'Graduate Java Developer', status:status8).save();
     
     def application1 = new Application(student:student1, jobTitle:placementOpportunity1, status:status3, dateCreated:new Date(), lastUpdated:new Date()).save();
     
-	def application2 = new Application(student:student2, jobTitle:placementOpportunity1, status:status3, dateCreated:new Date(), lastUpdated:new Date()).save();
+	def application2 = new Application(student:student2, jobTitle:placementOpportunity2, status:status8, dateCreated:new Date(), lastUpdated:new Date()).save();
     
-    def application3 = new Application(student:student3, jobTitle:placementOpportunity1, status:status3, dateCreated: new Date(), lastUpdated:new Date()).save();
+    def application3 = new Application(student:student3, jobTitle:placementOpportunity3, status:status1, dateCreated: new Date(), lastUpdated:new Date()).save();
     
-    def application4 = new Application(student:student4, jobTitle:placementOpportunity1, status:status3, dateCreated:new Date(), lastUpdated:new Date()).save();
+    def application4 = new Application(student:student4, jobTitle:placementOpportunity4, status:status8, dateCreated:new Date(), lastUpdated:new Date()).save();
     
-    def application5 = new Application(student:student5, jobTitle:placementOpportunity1, status:status3, dateCreated:new Date(), lastUpdated:new Date()).save();
+    def application5 = new Application(student:student5, jobTitle:placementOpportunity1, status:status8, dateCreated:new Date(), lastUpdated:new Date()).save();
     
     }
     def destroy = {
--- Placements/grails-app/controllers/placements/PlacementOpportunityController.groovy
+++ Placements/grails-app/controllers/placements/PlacementOpportunityController.groovy
@@ -1,6 +1,7 @@
 package placements
 
 import org.springframework.dao.DataIntegrityViolationException
+import grails.converters.*;
 
 class PlacementOpportunityController {
 
@@ -10,6 +11,26 @@ class PlacementOpportunityController {
         redirect(action: "list", params: params)
     }
 
+	def listOpenPlacements() {
+		def response = [:]
+		response.status = 'Open'
+		
+	def status = Status.findByCode('Open')
+		
+	def placements=PlacementOpportunity.findAllByStatus(status)
+		response.placements = []
+		placements.each{
+		response.placements.add([companyName:it.companyName, jobTitle:it.jobTitle])
+		
+		}
+		
+		withFormat {
+		html response
+		json { render response as JSON }
+		xml { render response as XML }
+		}
+	}
+	
     def list() {
         params.max = Math.min(params.max ? params.int('max') : 10, 100)
         [placementOpportunityInstanceList: PlacementOpportunity.list(params), placementOpportunityInstanceTotal: PlacementOpportunity.count()]
